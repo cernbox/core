42914daf81be52dbef924edb8e4a90d1c8359c18
Hugo Gonzalez Labrador <hglavra@gmail.com>
2015-04-28 14:34:59 +0200

    Added automatic homedir creation

--- lib/private/user/session.php
+++ lib/private/user/session.php
@@ -192,6 +192,11 @@ class Session implements IUserSession, Emitter {
 	 * @throws LoginException
 	 */
 	public function login($uid, $password) {
+		// HUGO we lower case the username to not having problems with LDAP case-insensitive username resolution
+		// This happened because the LDAP server is case-insensitive and the id resolution via nscd is case sensitive
+		// so the user is correclty authenticated but we reply with a wrong message saying the user should subscribe to a Linux e-group
+		$uid = strtolower($uid);
+
 		$this->manager->emit('\OC\User', 'preLogin', array($uid, $password));
 		$user = $this->manager->checkPassword($uid, $password);
 		if ($user !== false) {
@@ -200,11 +205,65 @@ class Session implements IUserSession, Emitter {
 					$this->setUser($user);
 					$this->setLoginName($uid);
 					$this->manager->emit('\OC\User', 'postLogin', array($user, $password));
-					if ($this->isLoggedIn()) {
+
+					// HUGO if EOS storage is not enabled we let the user enter as a normal guy
+					if (! \OC\Files\ObjectStore\EosUtil::getEosPrefix()) {
 						return true;
-					} else {
-						throw new LoginException('Login canceled by app');
 					}
+
+					// HUGO check user has valid uid and gid
+					// TODO cache uid in User class to speed the system
+					$uidAndGid = \OC\Files\ObjectStore\EosUtil::getUidAndGid($uid);
+					if($uidAndGid === false) {
+						\OCP\Util::writeLog('EOSBOOT', "user: $uid has not a valid uid", \OCP\Util::ERROR);
+			                        $tmpl = new \OC_Template('', 'error', 'guest');
+			                        $tmpl->assign('errors', array(1 => array('error' => "Your account has no computing group assigned. <br> Please use the CERN Account Service to fix this.  You may also check out <a href=\"http://cern.ch/cernbox-resources/faq.html\">CERNBOX FAQ</a> for additional information. <br> If the problem persists then please report it via CERN Service Portal.")));
+			                        $tmpl->printPage();
+			                        \OCP\User::logout();
+			                        exit();
+			                        return false;
+					}	
+					\OCP\Util::writeLog('EOSBOOT', "user: $uid has valid uid", \OCP\Util::ERROR);
+
+					// HUGO check user has valid homedir if not we create it
+					$homedir = \OC\Files\ObjectStore\EosUtil::getEosPrefix() . substr($uid, 0, 1) . "/" . $uid;
+					$meta = \OC\Files\ObjectStore\EosUtil::getFileByEosPath($homedir);
+
+					if($meta) { // path exists so we let the user access the system
+						\OCP\Util::writeLog('EOSBOOT', "user: $uid has a valid homedir that is: $homedir", \OCP\Util::ERROR);
+						return true;
+					}
+					
+					\OCP\Util::writeLog('EOSBOOT', "user: $uid does NOT have valid homedir that is:  $homedir", \OCP\Util::ERROR);
+					// the path does not exists so we create it				
+					// create home dir
+					$script_path = \OCP\Config::getSystemValue("eos_configure_new_homedir_script_path", false);
+                    if(!$script_path) {
+                        \OCP\Util::writeLog('EOSBOOT', "cannot find script for creating users. check config.php", \OCP\Util::ERROR);
+                        $tmpl = new \OC_Template('', 'error', 'guest');
+                        $tmpl->assign('errors', array(1 => array('error' => "Your account could not be created on the fly (internal script not found). <br> Please report via CERN Service Portal.")));
+                        $tmpl->printPage();
+                        \OCP\User::logout();
+                        exit();
+                        return false;
+                   }
+                   $result = null;
+                   $errcode = null;
+                   $cmd2 = "/bin/bash $script_path " . $uid;
+                   exec($cmd2, $result, $errcode);
+                   if($errcode !== 0){
+                           \OCP\Util::writeLog('EOSBOOT', "error running the script to create the homedir: $homedir for user: $uid$ CMD: $cmd2 errcode: $errcode", \OCP\Util::ERROR);
+                            $tmpl = new \OC_Template('', 'error', 'guest');
+                            $tmpl->assign('errors', array(1 => array('error' => "Your account could not be created on the fly. <br> Please check if your account has a computing group defined on <a href=\"http://cern.ch/account\">http://cern.ch/account</a>. <br> This may be achieved by subscribing to Linux and Lxplus services. <br> If the problem persists then please report it via CERN Service Portal.")));
+                            $tmpl->printPage();
+                            \OCP\User::logout();
+                            exit();
+                           return false;
+                    }
+                   \OCP\Util::writeLog('EOSBOOT', "homedir: $homedir created for user: $uid", \OCP\Util::ERROR);
+					
+					// all good, let the user enter
+					return true;
 				} else {
 					return false;
 				}
