5b641e85bdab791a26c16392615db60677ba41e1
Hugo Gonzalez Labrador <hglavra@gmail.com>
2015-05-06 11:54:15 +0200

    Added parsing of eos member

--- lib/private/files/objectstore/eosparser.php
+++ lib/private/files/objectstore/eosparser.php
@@ -153,30 +153,11 @@ class EosParser {
                 */
 
                 $fields = explode(" ", $line_to_parse);
-                $keylength = explode("=",$fields[8]);
-                $keylength = $keylength[1];
-                $realFile = explode("=",$fields[9]);
-                $realFile = implode("=",array_slice($realFile,1));
-                $total = strlen($realFile);
-                if($total != $keylength){
-                  $index = 10; $stop = false;
-                  while(!$stop){
-                    $realFile .= " " . $fields[$index];
-                    $total += strlen($fields[$index]) + 1;
-                    unset($fields[$index]);
-                    $index++;
-                    if($total == $keylength){
-                      $stop = true;
-                    }
-                  }
-                  $fields[9] = "restore-path=" . $realFile;
-                  $fields = array_values($fields);
-                }
                 foreach ($fields as $value) {
                         $splitted           = explode("=", $value);
-                        $info[$splitted[0]] = implode("=",array_slice($splitted,1));
+                        $info[$splitted[0]] = $splitted[1];
                 }
-                return $info;
+                return $info['member'] == "true" ? true : false;
 	}
 
 
