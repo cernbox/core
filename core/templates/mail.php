c72be739dbd7c220ea8a0710ed1994e68ed15265
nadir <nadir@nadir-TTL-TEKNOPRO>
2015-10-21 09:15:40 +0200

    Email notification fix
    Share author name will now appear on the email sent to share targets.

--- core/templates/mail.php
+++ core/templates/mail.php
@@ -12,7 +12,7 @@
 <td width="20px">&nbsp;</td>
 <td style="font-weight:normal; font-size:0.8em; line-height:1.2em; font-family:verdana,'arial',sans;">
 <?php
-print_unescaped($l->t('Hey there,<br><br>just letting you know that %s shared <strong>%s</strong> with you.<br><a href="%s">View it!</a><br><br>%s', array($_['user_displayname'], $_['filename'], $_['link'],$_['custom_info'])));
+print_unescaped($l->t('Hey there,<br><br>just letting you know that <b><font color="#58ACFA">%s</font></b> shared <strong>%s</strong> with you.<br><a href="%s">View it!</a><br><br>%s', array($_['user_displayname'], $_['filename'], $_['link'],$_['custom_info'])));
 if ( isset($_['expiration']) ) {
 	p($l->t("The share will expire on %s.", array($_['expiration'])));
 	print_unescaped('<br><br>');
