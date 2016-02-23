a161f1a237e8a43c5d21990a6d336c89447d0b35
nadir <tft105b@hotmail.com>
2016-02-23 10:12:11 +0100

    [pr|patch] Prevent from mounting unncessary fs on shares
    This patch will not be necessary at all with oc 9.0

--- apps/files_sharing/lib/mountprovider.php
+++ apps/files_sharing/lib/mountprovider.php
@@ -59,6 +59,68 @@ class MountProvider implements IMountProvider {
 	 * @return \OCP\Files\Mount\IMountPoint[]
 	 */
 	public function getMountsForUser(IUser $user, IStorageFactory $storageFactory) {
+		/** CERNBOX SHARE OPTIMIZATION PULL REQUEST PATCH */
+		// HUGO in an normal request this hook is called at least twice, reducing performance, so we cached
+		if (isset ( $GLOBALS ["shared_setup_hook"] )) {
+			return;
+		}
+		
+		// KUBA: avoid mounting all shared storages if not necessary:
+		// \OCP\Util::writeLog('KUBA',"PATH" . __FUNCTION__ . "($kuba_path) dir=".$_GET["dir"]." REQ_URI=".$_SERVER['REQUEST_URI'], \OCP\Util::ERROR);
+		
+		$mount_shared_stuff = false;
+		
+		if (isset($_GET['view']) && ($_GET ["view"] == "sharingin" or $_GET ["view"] == "sharingout" or $_GET ["view"] == "sharinglinks")) {
+			$mount_shared_stuff = true;
+		} else {
+			$uri_path_array = array ();
+				
+			if ($_SERVER ['REQUEST_METHOD'] === 'POST') {
+				if (isset ( $_POST ["dir"] )) {
+					array_push ( $uri_path_array, $_POST ["dir"] );
+				} else {
+					$mount_shared_stuff = true; // if dir is not defined, take the safe bet
+				}
+			} else {
+				if (isset ( $_GET ["dir"] )) {
+					array_push ( $uri_path_array, $_GET ["dir"] );
+				} else {
+					$mount_shared_stuff = true; // if dir is not defined, take the safe bet
+				}
+		
+				if (isset ( $_GET ["files"] )) {
+						
+					if (is_array ( $_GET ["files"] )) {
+						$uri_path_array = array_merge ( $uri_path_array, $_GET ["files"] );
+					} else {
+						array_push ( $uri_path_array, $_GET ["files"] );
+					}
+				}
+			}
+				
+			foreach ( $uri_path_array as $uri_path ) {
+				\OCP\Util::writeLog ( 'KUBA', "OPTIMIZATION" . __FUNCTION__ . " files=" . $_GET ["files"] . " uri_path=" . $uri_path . " ", \OCP\Util::ERROR );
+				if ($uri_path) {
+						
+					if (EosUtil::isProjectURIPath ( $uri_path )) {
+						$mount_shared_stuff = true;
+					} elseif (EosUtil::isSharedURIPath ( $uri_path )) {
+						$mount_shared_stuff = true;
+					}
+						
+					// \OCP\Util::writeLog('KUBA',"EosUtil::isSharedURIPath" . __FUNCTION__ . "uri_path=|${uri_path}| ->".EosUtil::isSharedURIPath($uri_path), \OCP\Util::ERROR);
+				}
+			}
+		}
+		
+		if (! $mount_shared_stuff) {
+			// \OCP\Util::writeLog('KUBA',"PATH" . __FUNCTION__ . "($kuba_path) dir=".$_GET["dir"]." NOT MOUNTING=".$mount_shared_stuff, \OCP\Util::ERROR);
+				
+			return; // OPTIMIZATION ON/OFF
+		}
+		
+		// \OCP\Util::writeLog('KUBA',"PATH" . __FUNCTION__ . "($kuba_path) dir=".$_GET["dir"]." MOUNTING=".$mount_shared_stuff, \OCP\Util::ERROR);
+		
 		$shares = \OCP\Share::getItemsSharedWithUser('file', $user->getUID());
 		$propagator = $this->propagationManager->getSharePropagator($user);
 		$propagator->propagateDirtyMountPoints($shares);
@@ -82,6 +144,9 @@ class MountProvider implements IMountProvider {
 			);
 		}, $shares);
 		// array_filter removes the null values from the array
+		/** CERNBOX SHARE OPTIMIZATION PULL REQUEST PATCH */
+		$GLOBALS ["shared_setup_hook"] = true;
+		
 		return array_filter($shares);
 	}
 }
