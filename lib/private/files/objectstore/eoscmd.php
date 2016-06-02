e91f136bea176a0d2f54f333b0b010d679a3d660
NadirRoGue <nadir.ro.gue@gmail.com>
2016-06-02 08:54:14 +0200

    [Patch] Adding a sleep interval between eos command retry

--- lib/private/files/objectstore/eoscmd.php
+++ lib/private/files/objectstore/eoscmd.php
@@ -15,16 +15,29 @@ class EosCmd {
 		$user = \OCP\User::getUser();
 		// Keep requesting EOS while we get a 22 error code
 		
-		$retrivalAttempts = \OC::$server->getConfig()->getSystemValue('eos_cmd_retry_attempts', -1);
-		$counter = 0;
-		do
+		$result = null;
+		$errcode = null;
+		exec($fullCmd, $result, $errcode);
+		
+		if($errcode === 22)
 		{
-			$result = null;
-			$errcode = null;
-			exec($fullCmd, $result, $errcode);
-			$counter++;
+			$retrivalAttempts = \OC::$server->getConfig()->getSystemValue('eos_cmd_retry_attempts', 3);
+			$infinite = $retrivalAttempts < 0;
+			$counter = 1;
+			do
+			{
+				// Sleep for half a second
+				usleep(500000);
+				
+				//Retry the command
+				$result = null;
+				$errcode = null;
+				exec($fullCmd, $result, $errcode);
+				
+				$counter++;
+			}
+			while($errcode === 22 && ($infinite || $counter < $retrivalAttempts));
 		}
-		while($errcode === 22 && $counter !== $retrivalAttempts);
 		
 		if($errcode === 0) {
 			\OCP\Util::writeLog('EOSCMD', "luser: $user cmd:$cmd errcode:$errcode", \OCP\Util::WARN);
