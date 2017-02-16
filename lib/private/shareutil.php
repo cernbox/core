cd4505db02414376487604e894534776c3652f19
Hugo Gonzalez Labrador <github@hugo.labkode.com>
2017-02-16 13:39:11 +0100

    Fix ancestor/children check when sharing

--- lib/private/shareutil.php
+++ lib/private/shareutil.php
@@ -4,43 +4,31 @@ namespace OC;
 
 use \OC\Files\ObjectStore\EosUtil;
 
-class ShareUtil
-{
-	public static function checkParentDirSharedById($fileId, $isShareByLink)
-	{
+class ShareUtil {
+	public static function checkParentDirSharedById($fileId, $isShareByLink) {
 		self::checkParentDirShared(EosUtil::getFileById($fileId), $isShareByLink);
-	}	
-	
-	public static function checkParentDirShared(array $eosMeta, $isShareByLink)
-	{
-		if($isShareByLink) {
+	}
+
+	public static function checkParentDirShared(array $eosMeta, $isShareByLink) {
+		if ($isShareByLink) {
 			return;
 		}
-		
-		if(!$eosMeta) {
+
+		if (!$eosMeta) {
 			throw \Exception('The file does not exist');
 		}
-		
+
 		$owner = EosUtil::getOwner($eosMeta['eospath']);
 		$currentPath = $eosMeta['path'];
-		$parentPath = dirname($eosMeta['path']);
-		
-		if(strpos($currentPath, 'files') === 0) {
-			$currentPath= substr($currentPath, 5);
+
+		if (strpos($currentPath, 'files') === 0) {
+			$currentPath = substr($currentPath, 5);
 		}
-		
-		if($currentPath=== '' || $currentPath=== '/') {
+
+		if ($currentPath === '' || $currentPath === '/') {
 			return;
 		}
-		
-		if(strpos($parentPath, 'files') === 0) {
-			$parentPath = substr($parentPath, 5);
-		}
-		
-		if($parentPath === '' || $parentPath === '/') {
-			return;
-		}
-		
+
 		/*
 		The new algorithm will apply this logic:
 		Ex: we want to share /test/L0/L1
@@ -54,19 +42,24 @@ class ShareUtil
 		2) Children folder X already shared
 		*/
 
-		// /test/L0, /test
-		$parentPaths = self::getParentFolders($parentPath);	
-	
-		$query = \OC_DB::prepare('SELECT file_target FROM oc_share WHERE uid_owner = ? AND share_type != 3');
+		$query = \OC_DB::prepare('SELECT * FROM oc_share WHERE uid_owner = ? AND share_type != 3');
 		$shares = $query->execute([$owner])->fetchAll();
-		
-		$sharedFolderPath = self::parentFoldersHaveBeenShared($parentPaths, $shares);
-		if($sharedFolderPath !== false) {
+		$allPaths = array();
+		foreach ($shares as $share) {
+			$fileID = $share['item_source'];
+			$meta = EosUtil::getFileById($fileID);
+			if ($meta) {
+				$allPaths[] = substr($meta['path'], 5); // remote files/ prefix
+			}
+		}
+
+		$sharedFolderPath = self::parentFoldersHaveBeenShared($allPaths, $currentPath);
+		if ($sharedFolderPath !== false) {
 			throw new \Exception("Unable to share the file because the ancestor directory '$sharedFolderPath' has been already shared");
 		}
-		
-		$sharedFolderPath = self::childrenFoldersHaveBeenShared($currentPath, $shares);		
-		if($sharedFolderPath) {
+
+		$sharedFolderPath = self::childrenFoldersHaveBeenShared($allPaths, $currentPath);
+		if ($sharedFolderPath) {
 			throw new \Exception("Unable to share the file because the subfolder '$sharedFolderPath' has been already shared");
 		}
 	}
@@ -75,41 +68,28 @@ class ShareUtil
 		$path = trim($path, "/");
 		$parentPaths = array();
 		$tokens = explode("/", $path);
-		foreach($tokens as $index => $token) {
-			$p = implode("/", array_slice($tokens, 0, $index + 1));	
+		foreach ($tokens as $index => $token) {
+			$p = implode("/", array_slice($tokens, 0, $index + 1));
 			$parentPaths[] = "/" . $p;
 		}
 		return $parentPaths;
 	}
 
-	private static function parentFoldersHaveBeenShared($parentFolders, $shares) {
-		$targets = array_map(function ($share) {
-			// file_targets of user shares end with a particular suffix.
-			// Ex: /test/L1 (#18400030) so we need to remove that suffix
-			// before comparing.
-			$fileTarget = $share['file_target'];
-			$fileTargetTokens = explode(" ", $fileTarget);
-			array_pop($fileTargetTokens); // remove suffix
-			$fileTarget = implode(" ", $fileTargetTokens);
-			return $fileTarget;
-		}, $shares);
-		foreach($parentFolders as $path) {
-			if (in_array($path, $targets)) {
+	private static function parentFoldersHaveBeenShared($allPaths, $currentPath) {
+		$parentPaths = self::getParentFolders(dirname($currentPath));
+		foreach($parentPaths as $path) {
+			if(in_array($path, $allPaths)) {
 				return $path;
 			}
 		}
 		return false;
 	}
-	
-	private static function childrenFoldersHaveBeenShared($pathToBeShared, $shares) {
-		$prefix = rtrim($pathToBeShared, "/") . "/";
-		foreach($shares as $share) {
-			$fileTarget = $share['file_target'];
-			if(strpos($fileTarget, $prefix) === 0) {
-				$fileTargetTokens = explode(" ", $fileTarget);
-				array_pop($fileTargetTokens); // remove suffix
-				$fileTarget = implode(" ", $fileTargetTokens);
-				return $fileTarget;
+
+	private static function childrenFoldersHaveBeenShared($allPaths, $currentPath) {
+		$currentPath = $currentPath . "/";
+		foreach ($allPaths as $path) {
+			if (strpos($path, $currentPath) === 0) {
+				return $path;
 			}
 		}
 		return false;
