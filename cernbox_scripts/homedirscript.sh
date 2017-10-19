2321ff9f54567d43e80d4a4864a0c195f51ed866
Hugo Gonzalez Labrador <github@hugo.labkode.com>
2017-10-19 14:34:50 +0200

    Send mail to new users

--- cernbox_scripts/homedirscript.sh
+++ cernbox_scripts/homedirscript.sh
@@ -63,3 +63,35 @@ eos -b -r 0 0 access allow user $usr # this is temporary until we allow all user
 #echo 'SUCCESS' $homedir 'created and ownership set to' $usr:$group
 
 # DONE.
+read -r -d '' mailtext <<- EOM
+	Dear $usr,
+
+	You have just subscribed to the CERNBox Service – welcome!
+
+	This service provides personal storage space that can be synchronised to desktops, laptops and mobile devices, as well as be shared with CERN users and non-CERN users.
+
+	Cordially,
+
+	CERNBox Service Team
+
+	CERNBox User Manual (http://cernbox.web.cern.ch/cernbox/en/)
+	CERNBox Ordered FAQs List (https://cern.service-now.com/service-portal/article.do?n=KB0004587)
+	CERNBox Support (https://cern.service-now.com/service-portal/report-ticket.do?name=request&se=CERNBox-Service)
+
+	=======================================================================
+
+	Cher $usr,
+
+	Vous venez de vous inscrire à CERNBox.
+
+	Ce service fournit un espace personnel de stockage pouvant être synchronisé entre ordinateurs, téléphones, tablettes ainsi qu'être partagé avec des utilisateurs du CERN ou extérieur au CERN.
+
+	Cordialement,
+
+	L'equipe CERNBox Service
+
+	CERNBox User Manual (http://cernbox.web.cern.ch/cernbox/en/)
+	CERNBox Ordered FAQs List (https://cern.service-now.com/service-portal/article.do?n=KB0004587)
+	CERNBox Support (https://cern.service-now.com/service-portal/report-ticket.do?name=request&se=CERNBox-Service)
+EOM
+echo "$mailtext" | mail -r "cernbox-noreply@cern.ch" -s "CERNBox Service" $usr@cern.ch
